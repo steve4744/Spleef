c27e194a714fc4a64f6484217a70b453644ce775
1wairesd <1wairesd.com.industries@gmail.com>
2025-10-20 17:44:23 -0100

    Refactor: replace deprecated GENERIC_MAX_HEALTH with attribute API and lower required version to 1.21

--- src/main/java/spleef/datahandler/PlayerDataStore.java
+++ src/main/java/spleef/datahandler/PlayerDataStore.java
@@ -39,183 +39,189 @@ import spleef.Spleef;
 
 public class PlayerDataStore {
 
-	private Map<String, ItemStack[]> plinv = new HashMap<>();
-	private Map<String, ItemStack[]> plarmor = new HashMap<>();
-	private Map<String, Collection<PotionEffect>> pleffects = new HashMap<>();
-	private Map<String, Location> plloc = new HashMap<>();
-	private Map<String, Integer> plhunger = new HashMap<>();
-	private Map<String, GameMode> plgamemode = new HashMap<>();
-	private Map<String, Integer> pllevel = new HashMap<>();
-	private Map<String, Double> plhealth = new HashMap<>();
-	private Map<String, Boolean> plflight = new HashMap<>();
-	private File file;
-	private final Spleef plugin;
+    private Map<String, ItemStack[]> plinv = new HashMap<>();
+    private Map<String, ItemStack[]> plarmor = new HashMap<>();
+    private Map<String, Collection<PotionEffect>> pleffects = new HashMap<>();
+    private Map<String, Location> plloc = new HashMap<>();
+    private Map<String, Integer> plhunger = new HashMap<>();
+    private Map<String, GameMode> plgamemode = new HashMap<>();
+    private Map<String, Integer> pllevel = new HashMap<>();
+    private Map<String, Double> plhealth = new HashMap<>();
+    private Map<String, Boolean> plflight = new HashMap<>();
+    private File file;
+    private final Spleef plugin;
 
-	public PlayerDataStore(Spleef plugin) {
-		this.plugin = plugin;
-		file = new File(plugin.getDataFolder(), "players.yml");
-		if (!file.exists()) {
-			try {
-				file.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public PlayerDataStore(Spleef plugin) {
+        this.plugin = plugin;
+        file = new File(plugin.getDataFolder(), "players.yml");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public void storePlayerInventory(Player player) {
-		PlayerInventory pinv = player.getInventory();
-		plinv.put(player.getName(), pinv.getContents());
-		pinv.clear();
-	}
+    public void storePlayerInventory(Player player) {
+        PlayerInventory pinv = player.getInventory();
+        plinv.put(player.getName(), pinv.getContents());
+        pinv.clear();
+    }
 
-	public void storePlayerFlight(Player player) {
-		plflight.put(player.getName(), player.getAllowFlight());
-	}
+    public void storePlayerFlight(Player player) {
+        plflight.put(player.getName(), player.getAllowFlight());
+    }
 
-	public void storePlayerArmor(Player player) {
-		PlayerInventory pinv = player.getInventory();
-		plarmor.put(player.getName(), pinv.getArmorContents());
-		pinv.setArmorContents(null);
-	}
+    public void storePlayerArmor(Player player) {
+        PlayerInventory pinv = player.getInventory();
+        plarmor.put(player.getName(), pinv.getArmorContents());
+        pinv.setArmorContents(null);
+    }
 
-	public void storePlayerPotionEffects(Player player) {
-		Collection<PotionEffect> peff = player.getActivePotionEffects();
-		pleffects.put(player.getName(), peff);
-		for (PotionEffect peffect : peff) {
-			player.removePotionEffect(peffect.getType());
-		}
-	}
+    public void storePlayerPotionEffects(Player player) {
+        Collection<PotionEffect> peff = player.getActivePotionEffects();
+        pleffects.put(player.getName(), peff);
+        for (PotionEffect peffect : peff) {
+            player.removePotionEffect(peffect.getType());
+        }
+    }
 
-	public void storePlayerLocation(Player player) {
-		plloc.put(player.getName(), player.getLocation());
-	}
+    public void storePlayerLocation(Player player) {
+        plloc.put(player.getName(), player.getLocation());
+    }
 
-	public void storePlayerHunger(Player player) {
-		plhunger.put(player.getName(), player.getFoodLevel());
-		if (plugin.getConfig().getBoolean("onjoin.fillhunger")) {
-			player.setFoodLevel(20);
-		}
-	}
+    public void storePlayerHunger(Player player) {
+        plhunger.put(player.getName(), player.getFoodLevel());
+        if (plugin.getConfig().getBoolean("onjoin.fillhunger")) {
+            player.setFoodLevel(20);
+        }
+    }
 
-	public void storePlayerGameMode(Player player) {
-		plgamemode.put(player.getName(), player.getGameMode());
-		player.setGameMode(getGameMode());
-	}
+    public void storePlayerGameMode(Player player) {
+        plgamemode.put(player.getName(), player.getGameMode());
+        player.setGameMode(getGameMode());
+    }
 
-	public void storePlayerLevel(Player player) {
-		pllevel.put(player.getName(), player.getLevel());
-		player.setLevel(0);
-	}
+    public void storePlayerLevel(Player player) {
+        pllevel.put(player.getName(), player.getLevel());
+        player.setLevel(0);
+    }
 
-	public void storePlayerHealth(Player player) {
-		LivingEntity le = (LivingEntity) player;
-		plhealth.put(player.getName(), le.getHealth());
-		if (plugin.getConfig().getBoolean("onjoin.fillhealth")) {
-			le.setHealth(le.getAttribute(Attribute.MAX_HEALTH).getValue());
-		}
-	}
+    public void storePlayerHealth(Player player) {
+        LivingEntity le = (LivingEntity) player;
+        plhealth.put(player.getName(), le.getHealth());
+        if (plugin.getConfig().getBoolean("onjoin.fillhealth")) {
+            Attribute maxHealthAttr = Attribute.valueOf("GENERIC_MAX_HEALTH");
+            if (le.getAttribute(maxHealthAttr) != null) {
+                le.setHealth(le.getAttribute(maxHealthAttr).getValue());
+            }
+        }
+    }
 
-	public void restorePlayerInventory(Player player) {
-		player.getInventory().setContents(plinv.remove(player.getName()));
-	}
+    public void restorePlayerInventory(Player player) {
+        player.getInventory().setContents(plinv.remove(player.getName()));
+    }
 
-	public void restorePlayerArmor(Player player) {
-		player.getInventory().setArmorContents(plarmor.remove(player.getName()));
-	}
+    public void restorePlayerArmor(Player player) {
+        player.getInventory().setArmorContents(plarmor.remove(player.getName()));
+    }
 
-	public void restorePlayerPotionEffects(Player player) {
-		player.addPotionEffects(pleffects.remove(player.getName()));
-	}
+    public void restorePlayerPotionEffects(Player player) {
+        player.addPotionEffects(pleffects.remove(player.getName()));
+    }
 
-	public void restorePlayerLocation(Player player) {
-		player.teleport(plloc.remove(player.getName()));
-	}
-	
-	public void restorePlayerFlight(Player player) {
-		player.setAllowFlight(plflight.get(player.getName()));
-	}
+    public void restorePlayerLocation(Player player) {
+        player.teleport(plloc.remove(player.getName()));
+    }
 
-	public void clearPlayerLocation(Player player) {
-		plloc.remove(player.getName());
-	}
+    public void restorePlayerFlight(Player player) {
+        player.setAllowFlight(plflight.get(player.getName()));
+    }
 
-	public void restorePlayerHunger(Player player) {
-		player.setFoodLevel(plhunger.remove(player.getName()));
-	}
+    public void clearPlayerLocation(Player player) {
+        plloc.remove(player.getName());
+    }
 
-	public void restorePlayerGameMode(Player player) {
-		player.setGameMode(plgamemode.remove(player.getName()));
-	}
+    public void restorePlayerHunger(Player player) {
+        player.setFoodLevel(plhunger.remove(player.getName()));
+    }
 
-	public void restorePlayerLevel(Player player) {
-		player.setLevel(pllevel.remove(player.getName()));
-	}
+    public void restorePlayerGameMode(Player player) {
+        player.setGameMode(plgamemode.remove(player.getName()));
+    }
 
-	public void restorePlayerHealth(Player player) {
-		LivingEntity le = (LivingEntity) player;
-		le.setHealth(Math.min(le.getAttribute(Attribute.MAX_HEALTH).getValue(), plhealth.remove(player.getName())));
-	}
+    public void restorePlayerLevel(Player player) {
+        player.setLevel(pllevel.remove(player.getName()));
+    }
 
-	public void saveDoubleJumpsToFile(OfflinePlayer player, int amount) {
-		String uuid = plugin.useUuid() ? player.getUniqueId().toString() : player.getName();
-		saveConfigFile(uuid, ".doublejumps", amount);
-	}
+    public void restorePlayerHealth(Player player) {
+        LivingEntity le = (LivingEntity) player;
+        Attribute maxHealthAttr = Attribute.valueOf("GENERIC_MAX_HEALTH");
+        if (le.getAttribute(maxHealthAttr) != null) {
+            le.setHealth(Math.min(le.getAttribute(maxHealthAttr).getValue(), plhealth.remove(player.getName())));
+        }
+    }
 
-	public void setWinStreak(OfflinePlayer player, int amount) {
-		String uuid = plugin.useUuid() ? player.getUniqueId().toString() : player.getName();
-		saveConfigFile(uuid, ".winstreak", amount);
-	}
+    public void saveDoubleJumpsToFile(OfflinePlayer player, int amount) {
+        String uuid = plugin.useUuid() ? player.getUniqueId().toString() : player.getName();
+        saveConfigFile(uuid, ".doublejumps", amount);
+    }
 
-	private void saveConfigFile(String uuid, String path, int amount) {
-		FileConfiguration config = YamlConfiguration.loadConfiguration(file);
-		if (amount == 0) {
-			config.set(uuid + path, null);
-			ConfigurationSection section = config.getConfigurationSection(uuid);
-			if (section != null && section.getKeys(false).isEmpty()) {
-				config.set(uuid, null);
-			}
+    public void setWinStreak(OfflinePlayer player, int amount) {
+        String uuid = plugin.useUuid() ? player.getUniqueId().toString() : player.getName();
+        saveConfigFile(uuid, ".winstreak", amount);
+    }
 
-		} else {
-			config.set(uuid + path, amount);
-		}
-		try {
-			config.save(file);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private void saveConfigFile(String uuid, String path, int amount) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+        if (amount == 0) {
+            config.set(uuid + path, null);
+            ConfigurationSection section = config.getConfigurationSection(uuid);
+            if (section != null && section.getKeys(false).isEmpty()) {
+                config.set(uuid, null);
+            }
 
-	public int getDoubleJumpsFromFile(OfflinePlayer player) {
-		String uuid = plugin.useUuid() ? player.getUniqueId().toString() : player.getName();
-		return getDoubleJumpsFromFile(uuid);
-	}
+        } else {
+            config.set(uuid + path, amount);
+        }
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public int getDoubleJumpsFromFile(String name) {
-		FileConfiguration config = YamlConfiguration.loadConfiguration(file);
-		return config.getInt(name + ".doublejumps", 0);
-	}
+    public int getDoubleJumpsFromFile(OfflinePlayer player) {
+        String uuid = plugin.useUuid() ? player.getUniqueId().toString() : player.getName();
+        return getDoubleJumpsFromFile(uuid);
+    }
 
-	public int getWinStreak(OfflinePlayer player) {
-		String uuid = plugin.useUuid() ? player.getUniqueId().toString() : player.getName();
-		return getWinStreakFromFile(uuid);
-	}
+    public int getDoubleJumpsFromFile(String name) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+        return config.getInt(name + ".doublejumps", 0);
+    }
 
-	private int getWinStreakFromFile(String name) {
-		FileConfiguration config = YamlConfiguration.loadConfiguration(file);
-		return config.getInt(name + ".winstreak", 0);
-	}
+    public int getWinStreak(OfflinePlayer player) {
+        String uuid = plugin.useUuid() ? player.getUniqueId().toString() : player.getName();
+        return getWinStreakFromFile(uuid);
+    }
 
-	public boolean hasStoredDoubleJumps(Player player) {
-		return getDoubleJumpsFromFile(player) > 0;
-	}
+    private int getWinStreakFromFile(String name) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+        return config.getInt(name + ".winstreak", 0);
+    }
 
-	private GameMode getGameMode() {
-		String gamemode = "SURVIVAL";
-		/*String gamemode = plugin.getConfig().getString("gamemode", "SURVIVAL");
+    public boolean hasStoredDoubleJumps(Player player) {
+        return getDoubleJumpsFromFile(player) > 0;
+    }
+
+    private GameMode getGameMode() {
+        String gamemode = "SURVIVAL";
+        /*String gamemode = plugin.getConfig().getString("gamemode", "SURVIVAL");
 		if (!gamemode.equalsIgnoreCase("ADVENTURE")) {
 			gamemode = "SURVIVAL";
 		}*/
-		return GameMode.valueOf(gamemode.toUpperCase());
-	}
+        return GameMode.valueOf(gamemode.toUpperCase());
+    }
 }
